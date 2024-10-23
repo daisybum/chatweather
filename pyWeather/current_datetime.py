92707c15aad73a1b16ad52a2a54f62f232024dd4
SanghyunPark <shpark@mv-w.com>
2024-10-23 11:42:36 +0900

    🧹 CHORE: 날짜 시간 출력에서 날짜만 출력으로 변경

--- pyWeather/current_datetime.py
+++ pyWeather/current_datetime.py
@@ -2,5 +2,5 @@ from datetime import datetime, timedelta
 
 
 def get_current_date():
-    current_date = datetime.now().date()
+    current_date = datetime.now()
     return current_date.strftime("%Y%m%d")
